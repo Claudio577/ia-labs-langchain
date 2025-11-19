c5030c417140556268a9d128aaf9caed826078fe
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 10:17:22 -0300

    Update agente_juridico.py

--- agents/agente_juridico.py
+++ agents/agente_juridico.py
@@ -3,14 +3,15 @@ from chains.summarizer import chain_resumo
 from config import get_llm
 from langchain.agents import initialize_agent, AgentType
 
+
 def criar_agente_juridico():
     llm = get_llm()
 
     tools = [
         Tool(
-            name="Análise Jurídica",
+            name="Resumo Jurídico",
             func=chain_resumo,
-            description="Resume e simplifica documentos jurídicos."
+            description="Gera resumos focados em documentos legais."
         )
     ]
 
