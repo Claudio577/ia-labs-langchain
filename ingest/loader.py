b157b3c70e6d58ebb7c5afd987be7afb29cb4ac6
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 09:08:11 -0300

    Update loader.py

--- ingest/loader.py
+++ ingest/loader.py
@@ -2,22 +2,22 @@ from pypdf import PdfReader
 from docx import Document
 
 def carregar_documentos(files):
-    documentos = []
+    textos = []
 
     for f in files:
         if f.name.endswith(".pdf"):
             reader = PdfReader(f)
-            text = ""
+            texto = ""
             for page in reader.pages:
-                text += page.extract_text() or ""
-            documentos.append(text)
+                texto += page.extract_text() or ""
+            textos.append(texto)
 
         elif f.name.endswith(".docx"):
             doc = Document(f)
-            text = "\n".join([p.text for p in doc.paragraphs])
-            documentos.append(text)
+            texto = "\n".join([p.text for p in doc.paragraphs])
+            textos.append(texto)
 
         elif f.name.endswith(".txt"):
-            documentos.append(f.read().decode("utf-8"))
+            textos.append(f.read().decode("utf-8"))
 
-    return documentos
+    return textos
