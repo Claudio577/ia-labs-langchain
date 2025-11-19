f3d0b01ded4dbec96a458b307151cf8bfd3ef6d8
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 08:51:37 -0300

    Update vector_store.py

--- ingest/vector_store.py
+++ ingest/vector_store.py
@@ -1,22 +1,11 @@
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
-from config import CHROMA_PATH, EMBEDDING_MODEL
 
-def criar_vector_store(docs):
-    embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
+def criar_vector_store(chunks):
+    embeddings = OpenAIEmbeddings()
 
-    vectordb = Chroma.from_documents(
-        documents=docs,
+    Chroma.from_texts(
+        texts=chunks,
         embedding=embeddings,
-        persist_directory=CHROMA_PATH
-    )
-
-    return vectordb
-
-def carregar_vector_store():
-    embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
-
-    return Chroma(
-        persist_directory=CHROMA_PATH,
-        embedding_function=embeddings
+        collection_name="base_corporativa"
     )
