8d1171f51c924063364bfad5776b67db0889a9b8
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 08:51:13 -0300

    Update splitter.py

--- ingest/splitter.py
+++ ingest/splitter.py
@@ -1,8 +1,13 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def dividir_documentos(docs):
+def dividir_documentos(textos):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
+        chunk_size=1500,
         chunk_overlap=150
     )
-    return splitter.split_documents(docs)
+
+    chunks = []
+    for t in textos:
+        chunks.extend(splitter.split_text(t))
+
+    return chunks
