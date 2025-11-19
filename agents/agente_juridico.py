b3705eee9138188406dd2d1ee803517d2a08ed61
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 10:07:34 -0300

    Update agente_juridico.py

--- agents/agente_juridico.py
+++ agents/agente_juridico.py
@@ -1,34 +1,25 @@
-from langchain.agents import AgentExecutor, create_react_agent
 from langchain.tools import Tool
 from chains.summarizer import chain_resumo
 from config import get_llm
-from langchain_core.prompts import PromptTemplate
+from langchain.agents import initialize_agent, AgentType
 
 def criar_agente_juridico():
     llm = get_llm()
 
     tools = [
         Tool(
-            name="Resumo Jurídico",
+            name="Análise Jurídica",
             func=chain_resumo,
-            description="Simplifica e resume conteúdos jurídicos."
+            description="Resume e simplifica documentos jurídicos."
         )
     ]
 
-    prompt = PromptTemplate(
-        template="""
-Você é um advogado corporativo especializado em compliance, contratos e legislação.
-Explique com precisão, linguagem clara e sem emitir parecer jurídico formal.
-
-Pergunta do usuário: {input}
-""",
-        input_variables=["input"],
-    )
-
-    agent = create_react_agent(
-        llm=llm,
+    agent = initialize_agent(
         tools=tools,
-        prompt=prompt
+        llm=llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=False,
+        handle_parsing_errors=True
     )
 
-    return AgentExecutor(agent=agent, tools=tools, verbose=False)
+    return agent
